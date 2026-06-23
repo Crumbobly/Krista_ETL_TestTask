5bbdabbbf37afc6207bfb7f76ff225510f9a882b
Valerii Krymov <vkvalera004@gmail.com>
2026-06-23 11:37:53 +0300

    Универсализирован FileUtils

--- src/main/java/ru/lab/utils/FileUtils.java
+++ src/main/java/ru/lab/utils/FileUtils.java
@@ -1,12 +1,24 @@
 package ru.lab.utils;
 
-import sun.misc.IOUtils;
-
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.charset.StandardCharsets;
 
 public class FileUtils {
+
+    private static byte[] toByteArray(InputStream is) throws IOException {
+        final ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+        byte[] data = new byte[4096];
+        int nRead;
+
+        while ((nRead = is.read(data, 0, data.length)) != -1) {
+            buffer.write(data, 0, nRead);
+        }
+
+        buffer.flush();
+        return buffer.toByteArray();
+    }
 
     public static String readFile(String path){
         try (final InputStream inputStream = FileUtils.class.getClassLoader().getResourceAsStream(path)) {
@@ -14,7 +26,7 @@ public class FileUtils {
             if (inputStream == null) {
                 throw new RuntimeException(path + " not found");
             }
-            return new String(IOUtils.readAllBytes(inputStream), StandardCharsets.UTF_8);
+            return new String(toByteArray(inputStream), StandardCharsets.UTF_8);
 
         } catch (IOException e) {
             throw new RuntimeException(e);
